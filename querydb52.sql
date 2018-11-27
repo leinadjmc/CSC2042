8ff9d0ece9324e3567ddf83d72e572f18dd64ada
Emma Burns <Emmrargh@gmail.com>
2018-11-27 18:57:10 +0000

    Added Query 1 and 2 from docs

--- querydb52.sql
+++ querydb52.sql
@@ -3,4 +3,33 @@ FROM person
 LEFT JOIN apartment ON person.person_id = apartment.person_id
 LEFT JOIN building ON apartment.building_number = building.building_number
 WHERE apartment.building_number = building.building_number AND apartment.postcode = building.postcode
-ORDER BY person.person_id
+ORDER BY person.person_id;
+
+SELECT apartment.apartment_number, apartment.building_number, apartment.postcode
+FROM apartment
+LEFT JOIN leaseagreement ON leaseagreement.apartment_number = apartment.apartment_number AND leaseagreement.building_number = apartment.building_number AND leaseagreement.postcode = apartment.postcode
+WHERE leaseagreement.lease_id IS NULL OR leaseagreement.lease_id NOT IN (
+SELECT t.lease_id
+FROM leaseagreement t
+INNER JOIN (
+    SELECT lease_id, max(end_date) AS MaxDate
+    FROM leaseagreement
+    WHERE expired = 0
+    GROUP BY lease_id
+) tm ON t.lease_id = tm.lease_id AND t.end_date = tm.MaxDate);
+
+(SELECT leaseagreement.monthly_rent, leaseagreement.lease_id, apartment.apartment_number, apartment.building_number, apartment.postcode, apartment.number_bedrooms, apartment.total_bathrooms, apartment.total_area
+FROM leaseagreement
+NATURAL JOIN apartment
+WHERE leaseagreement.expired = '0' AND leaseagreement.monthly_rent NOT IN (
+  SELECT A.monthly_rent
+  FROM leaseagreement A, leaseagreement B
+  WHERE A.monthly_rent < B.monthly_rent))
+UNION
+(SELECT leaseagreement.monthly_rent, leaseagreement.lease_id, apartment.apartment_number, apartment.building_number, apartment.postcode, apartment.number_bedrooms, apartment.total_bathrooms, apartment.total_area
+FROM leaseagreement
+NATURAL JOIN apartment
+WHERE leaseagreement.expired = '0' AND leaseagreement.monthly_rent NOT IN (
+  SELECT B.monthly_rent
+  FROM leaseagreement A, leaseagreement B
+  WHERE A.monthly_rent < B.monthly_rent));
