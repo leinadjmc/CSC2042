d35c9895cbd076167801dfe1a8f7dbee47125ad4
arm742 <45199725+arm742@users.noreply.github.com>
2018-11-27 17:14:41 +0000

    First Query Added

--- querydb52.sql
+++ querydb52.sql
@@ -1,1 +1,6 @@
-
+SELECT person.person_id, person.first_name, person.last_name, building.building_number, apartment.apartment_number, building.street, building.city, building.postcode
+FROM person
+LEFT JOIN apartment ON person.person_id = apartment.person_id
+LEFT JOIN building ON apartment.building_number = building.building_number
+WHERE apartment.building_number = building.building_number AND apartment.postcode = building.postcode
+ORDER BY person.person_id
